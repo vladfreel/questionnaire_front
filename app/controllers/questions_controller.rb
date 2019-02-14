8c6bf65e280fde3cff2e96128c7dfd944286f0fd
vladfreel <vladfreelmaster@gmail.com>
2019-02-14 10:55:54 +0200

    add index questions with styles

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,6 @@ class QuestionsController < ApplicationController
   def index
     questions = RestClient.get("http://#{set_host}/api/v1/questions.json", { accept: :json }).body
     @questions = JSON.parse(questions)
-    p @questions
   end
 
 end
